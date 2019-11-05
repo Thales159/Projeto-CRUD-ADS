e6fcd6c351fc4c75f97b7bb088746b7e2b33da85
Thales <thales.dasilvas@gmail.com>
2019-11-05 20:56:41 -0200

    CRUD Inicial

---
+++ ProjetoCRUD/src/univs/edu/usuario/Usuario.java
@@ -0,0 +1,70 @@
+package univs.edu.usuario;
+
+public class Usuario {
+    
+    private int idUsuario;
+    
+    private String nome;
+    
+    private String login;
+    
+    private String senha;
+    
+
+    /**
+     * @return the idUsuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+}
